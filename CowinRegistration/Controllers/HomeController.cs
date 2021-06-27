6bd816c742a4fee827fc6119b73c560022f2f73c
nishma1995 <nishmakk@gmail.com>
2021-06-27 16:16:00 -0700

    Add project files.

---
+++ CowinRegistration/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using CowinRegistration.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CowinRegistration.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index1(User  user)
+        {if (ModelState.IsValid)
+            {
+                string myConnection = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                SqlConnection connection = new SqlConnection(myConnection);
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("UserRegistration", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("Name", user.Name);
+                command.Parameters.AddWithValue("Age", user.Age);
+                command.Parameters.AddWithValue("State", user.State);
+                command.Parameters.AddWithValue("District", user.District);
+                command.Parameters.AddWithValue("AadharNumber", user.AadhaarNumber);
+                command.Parameters.AddWithValue("ChooseFromVendor", user.ChooseFromVendor);
+
+                command.ExecuteNonQuery();
+                connection.Close();
+
+            }
+
+            return View("View");
+        }
+    }
+}
---
+++ CowinRegistration/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CowinRegistration.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string District { get; set; }
+        [Required]
+        public string AadhaarNumber { get; set; }
+        [Required]
+        public string ChooseFromVendor { get; set; }
+    }
+    public enum Vendors
+    {
+        Covishield
+        ,Covaxine
+        ,Sputnik
+    }
+}
